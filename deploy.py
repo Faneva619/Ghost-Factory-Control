a1981f06538e3ad9daa58337ad90d810365bd8f7
Faneva RAVELONOMENJANAHARY <fanevaravelonomenjanahary@gmail.com>
2026-04-22 12:19:21 +0200

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,22 +2,21 @@ import os
 from github import Github
 import brain
 
-# On utilise os.getenv pour lire la clé cachée dans les Secrets de GitHub
-# SANS jamais l'écrire en texte clair ici !
+
 token = os.getenv('MY_GITHUB_TOKEN')
 
 g = Github(token)
 user = g.get_user()
 
-# On génère l'idée via le script brain.py
+
 domaine, tache = brain.get_random_project()
 repo_name = f"Project-{domaine}-{tache.replace(' ', '-')}"
 
 try:
-    # Création du dépôt sur ton compte
+    
     repo = user.create_repo(repo_name, description=f"Projet automatique : {tache}", auto_init=True)
     
-    # Création du fichier README
+    
     readme_content = f"# {tache}\nDomaine : {domaine}\n\nGénéré par Ghost-Factory."
     repo.create_file("README.md", "Initial commit", readme_content)
     
